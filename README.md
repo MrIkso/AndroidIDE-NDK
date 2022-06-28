6d64b25c70572510585621b6080ce5faa657bf51
MrIkso <mrikso821@gmail.com>
2022-06-28 12:31:25 +0300

    Add cmake and install script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# AndroidIDE-NDk
+# AndroidIDE-NDK
+This bash script will install ndk r24 by jzinferno to AndroidIDE app.
+- Install NDK-r24 for AndroidIDE in one command :
+```sh
+wget https://github.com/MrIkso/AndroidIDE-NDK/raw/main/ndk-install.sh && chmod +x ndk-install.sh && ./ndk-install.sh
+```
+Thanks to jzinferno and Lzhiyong
---
+++ ndk-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Script to install NDK into AndroidIDE
+install_dir=$HOME
+sdk_dir=$install_dir/android-sdk
+ndk_dir=$sdk_dir/ndk/24.0.8215888
+
+echo 'Warning! This NDK only for aarch64'
+cd $install_dir
+# download NDK
+echo 'Downloading NDK r24'
+wget https://github.com/jzinferno/termux-ndk/releases/download/v1/android-ndk-r24-aarch64.zip 
+echo 'Unziping NDK r24'
+unzip android-ndk-r24-aarch64.zip 
+rm android-ndk-r24-aarch64.zip
+# moving ndk to Android SDK directory
+mkdir $sdk_dir/ndk 
+mv android-ndk-r24 $sdk_dir/ndk/24.0.8215888 
+
+# create missing link
+ln -s $ndk_dir/toolchains/llvm/prebuilt/linux-aarch64 $ndk_dir/toolchains/llvm/prebuilt/linux-x86_64
+ln -s $ndk_dir/prebuilt/linux-aarch64 $ndk_dir/prebuilt/linux-x86_64
+
+# pathing cmake config
+sed -i 's/if(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/if(CMAKE_HOST_SYSTEM_NAME STREQUAL Android)\nset(ANDROID_HOST_TAG linux-aarch64)\nelseif(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/g' $ndk_dir/build/cmake/android-legacy.toolchain.cmake
+sed -i 's/if(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/if(CMAKE_HOST_SYSTEM_NAME STeREQUAL An   droid)\nset(ANDROID_HOST_TAG linux-aarch64)\nelseif(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/g' $ndk_dir/build/cmake/android.toolchain.cmake.patch
+
+# download cmake
+wget https://github.com/MrIkso/AndroidIDE-NDK/raw/main/cmake.zip
+# unzip cmake
+unzip cmake.zip -d $sdk_dir
+rm cmake.zip
+# set executable permission for cmake
+chmod -R +x $sdk_dir/cmake/bin
+# add cmake to path
+ideenv -s
+PATH=$PATH:$HOME/android-sdk/cmake/bin
+
+echo 'Installation Finished. Ndk has been installed successfully!'
