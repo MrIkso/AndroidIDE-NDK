7afc19f1b917e9833575757461e9614e5ae12023
Mr Ikso <mrikso821@gmail.com>
2022-11-06 20:40:34 +0200

    added selection with ndk version need to install

--- ndk-install.sh
+++ ndk-install.sh
@@ -3,32 +3,70 @@
 # Script to install NDK into AndroidIDE
 install_dir=$HOME
 sdk_dir=$install_dir/android-sdk
-ndk_dir=$sdk_dir/ndk/24.0.8215888
 cmake_dir=$sdk_dir/cmake
+
+ndk_dir=""
+ndk_ver=""
+ndk_ver_name=""
+
+echo "Select with NDK you need install?"
+
+select item in r21e r24 Quit
+do
+    case $item in
+        "r21e")
+            ndk_ver="21.4.7075529";
+            ndk_ver_name="r21e";
+            echo "Selected $ndk_ver_name ($ndk_ver)";
+            break
+            ;;
+            
+        "r24")
+            ndk_ver="24.0.8215888";
+            ndk_ver_name="r24";
+            echo "Selected $ndk_ver_name ($ndk_ver)";
+            break;;
+            
+        "Quit")
+           echo "Exit.."
+           exit
+           break;;
+        *)
+           echo "Ooops";;
+    esac
+done
 
 echo 'Warning! This NDK only for aarch64'
 cd $install_dir
-# checking if previous installed NDK r24 and cmake 
+# checking if previous installed NDK and cmake 
 
-if [ -d "$ndk_dir" ]; then
-    echo "$ndk_dir exists. Deleting NDK r24..."
+ndk_dir=$sdk_dir/ndk/$ndk_ver
+
+if [ -d "$ndk_dir" ]
+then
+    echo "$ndk_dir exists. Deleting NDK $ndk_ver..."
     rm -rf $ndk_dir
+else
+    echo "NDK does not exists."
 fi
 
-if [ -d "$cmake_dir/3.23.1" ]; then
+if [ -d "$cmake_dir/3.23.1" ]
+then
     echo "$cmake_dir/3.23.1 exists. Deleting cmake..."
     rm -rf $cmake_dir/3.23.1
+else
+    echo "Cmake does not exists."
 fi
 
 # download NDK
-echo 'Downloading NDK r24'
-wget https://github.com/jzinferno/termux-ndk/releases/download/v1/android-ndk-r24-aarch64.zip 
-echo 'Unziping NDK r24'
-unzip android-ndk-r24-aarch64.zip 
-rm android-ndk-r24-aarch64.zip
+echo "Downloading NDK $ndk_ver_name"
+wget https://github.com/jzinferno/termux-ndk/releases/download/v1/android-ndk-$ndk_ver_name-aarch64.zip 
+echo "Unziping NDK $ndk_ver_name"
+unzip android-ndk-$ndk_ver_name-aarch64.zip
+rm android-ndk-$ndk_ver_name-aarch64.zip
 # moving ndk to Android SDK directory
-mkdir $sdk_dir/ndk 
-mv android-ndk-r24 $ndk_dir
+mkdir $sdk_dir/ndk
+mv android-ndk-$ndk_ver_name $ndk_dir
 
 # create missing link
 ln -s $ndk_dir/toolchains/llvm/prebuilt/linux-aarch64 $ndk_dir/toolchains/llvm/prebuilt/linux-x86_64
