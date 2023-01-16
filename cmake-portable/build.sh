d491389b2cbbd398a817baf1e19c2aaa222f8581
Yaroslav <43380144+MrIkso@users.noreply.github.com>
2023-01-16 20:10:13 +0200

    Update build.sh

--- cmake-portable/build.sh
+++ cmake-portable/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1c511d09516af493694ed9baf13c55947a36389674d657a2d5e0ccedc6b291d8
 TERMUX_PKG_BUILD_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, zlib, libandroid-spawn"
+TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_RECOMMENDS="clang, make"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DSPHINX_MAN=ON
